f51221bfb422a8b72d9dc4f01315c2d4097e302c
Alessio1989 <alessiot89@users.noreply.github.com>
2024-02-11 17:32:53 +0100

    bump copyright and authors

--- src/ARKServerManager/Properties/AssemblyInfo.cs
+++ src/ARKServerManager/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Windows;
 [assembly: AssemblyTitle("ARK: Survival Evolved™ Server Manager")]
 [assembly: AssemblyDescription("The server manager makes it easy to setup and manage your ARK: Survival Evolved™ dedicated servers.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bletch1971")]
+[assembly: AssemblyCompany("Bletch1971,Alessio1989")]
 [assembly: AssemblyProduct("Server Managers")]
-[assembly: AssemblyCopyright("Copyright © 2015-2022")]
+[assembly: AssemblyCopyright("Copyright © 2015-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
